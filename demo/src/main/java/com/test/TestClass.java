bcb130440a35755a74887ec9abf3e6f7ad98470f
Soumendu Laha <67701082+soumendulaha@users.noreply.github.com>
2023-09-26 23:17:28 +0530

    Add files via upload

---
+++ demo/src/main/java/com/test/TestClass.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is a test class");
+
+	}
+
+}
