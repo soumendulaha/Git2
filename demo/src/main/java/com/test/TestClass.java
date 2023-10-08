14da56d40468ba4485f256602be082f0534a38be
Soumendu Laha <67701082+soumendulaha@users.noreply.github.com>
2023-10-08 12:29:20 +0530

    Change 1

--- demo/src/main/java/com/test/TestClass.java
+++ demo/src/main/java/com/test/TestClass.java
@@ -5,11 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("This is a test class");
-		System.out.println("Clone kora hoe gche");
-		System.out.println("Now adding something from github");
-		System.out.println("DHINKACHIKA DHINKAHIKA");
-		System.out.println("Now testing ");
+		//Change One
 	
 		
 
